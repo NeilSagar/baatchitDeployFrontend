8e99b4a5f5b3b5feb61a983bcf4b15d8caf9d629
Neil Sagar Sahu <neilsagar55555@gmail.com>
2024-02-08 00:24:53 +0530

    minor

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+// const url = "http://localhost:5000";
+const url ="https://baatchitbackent.onrender.com";
 
 export const handleRegister=async (userDetails)=>{
     try {
